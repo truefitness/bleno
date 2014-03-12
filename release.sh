e65962f60e49ad5c1ea173197a69cedf1868439d
Robert Hedges <rhedges@truefitness.com>
2014-03-12 14:31:55 -0500

    build

--- release.sh
+++ release.sh
@@ -4,5 +4,5 @@ git-dch -R
 git commit -a -m "build"
 export DEB_HOST_ARCH=i386
 export ARCH=i386
-export DIST=PRECISE
+export DIST=precise
 git-buildpackage --git-pbuilder --git-arch=$ARCH --git-dist=$DIST --git-tag
